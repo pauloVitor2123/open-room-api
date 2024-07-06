f424087cc72fde307ebd617d41de73673505195a
Paulo <paulo.vitor@jovensgenios.com>
2024-07-06 02:24:06 -0300

    feat: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { OfficeResolver } from './modules/offices/resolvers/office.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
+  providers: [OfficeResolver],
+})
+export class AppModule {}
